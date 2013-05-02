baa82f9368e88f7788a650433aab03cd796c323a
ThiagoNOliveira <eno.thiago@gmail.com>
2013-05-02 13:18:28 -0300

    Interfaces IPatientDTO and TreatmentDTO

---
+++ EHR.CoreShared/IPatientDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace EHR.CoreShared
+{
+    [ProtoContract(SkipConstructor = true)]
+    //[ProtoInclude(7, typeof(PatientDTO))]
+    public interface IPatientDTO
+    {
+        [ProtoMember(1)]
+        string Id { get; set; }
+        [ProtoMember(2)]
+        string Name { get; set; }
+        [ProtoMember(3)]
+        DateTime? DateBirthday { get; set; }
+        [ProtoMember(4)]
+        string CPF { get; set; }
+        [ProtoMember(5)]
+        string Identity { get; set; }
+        [ProtoMember(6)]
+        DbEnum Hospital { get; set; }
+        [ProtoMember(7)]
+        List<string> Records { get; set; }
+
+        string GetCPF();
+        void AddRecord(string record);
+
+
+        void AddTreatments(IList<ITreatmentDTO> treatments);
+    }
+}
---
+++ EHR.CoreShared/ITreatment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProtoBuf;
+
+namespace EHR.CoreShared
+{
+    [ProtoContract(SkipConstructor = true)]
+    public interface ITreatmentDTO
+    {
+        [ProtoMember(1)]
+        string Id { get; set; }
+        [ProtoMember(2)]
+        DateTime EntryDate { get; set; }
+        [ProtoMember(3)]
+        DateTime CheckOutDate { get; set; }
+    }
+}
