7eb2c600d365453c8769b135eb73f5d64e7e7b07
SammuelGarcia <sammuel_garcia@hotmail.com>
2013-05-16 16:40:31 -0300

    Implementacao cid/Def/Tus DTO

---
+++ CidDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHR.CoreShared
+{
+    public class CidDTO
+    {
+        public virtual short Id { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Code { get; set; }
+    }
+}
---
+++ DefDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EHR.CoreShared
+{
+    public class DefDTO
+    {
+        public virtual short Id { get; set; }
+        public virtual string Description { get; set; }
+    }
+}
