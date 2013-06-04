ccba5dd28dbdf30931808f869e75ea374d4d4e2e
ThiagoNOliveira <eno.thiago@gmail.com>
2013-06-04 12:28:59 -0300

    Adição da description do DBEnum

--- DbEnum.cs
+++ DbEnum.cs
@@ -15,20 +15,28 @@ namespace EHR.CoreShared
         [ProtoEnum(Name = "Sumario", Value = 2)]
         sumario,
         [ProtoEnum(Name = "QuintaDor", Value = 4)]
+        [Description("Quinta D'Or")]
         QuintaDor,
         [ProtoEnum(Name = "Pronto", Value = 5)]
+        [Description("Prontolinda")]
         Pronto,
         [ProtoEnum(Name = "Rios", Value = 6)]
+        [Description("Rios D'Or")]
         Rios,
         [ProtoEnum(Name = "Norte", Value = 7)]
+        [Description("Norte D'Or")]
         Norte,
         [ProtoEnum(Name = "Esperança", Value = 8)]
+        [Description("Esperança")]
         Esperanca,
         [ProtoEnum(Name = "Bangu", Value = 9)]
+        [Description("Bangu")]
         Bangu,
         [ProtoEnum(Name = "Copa", Value = 10)]
+        [Description("Copa D'Or")]
         Copa,
         [ProtoEnum(Name = "SM", Value = 11)]
+        [Description("São Marcos")]
         SM
     }
 }
