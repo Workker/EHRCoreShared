98b5a7d38b098a052ca6cfc09f2e4a6d71a80f33
ThiagoNOliveira <eno.thiago@gmail.com>
2013-08-23 19:13:52 -0300

    Adicionando os outros hospitais no DBEnum

--- DbEnum.cs
+++ DbEnum.cs
@@ -8,36 +8,61 @@ namespace EHR.CoreShared
     [ProtoContract(SkipConstructor = true)]
     public enum DbEnum : short
     {
-        //View de atendimento não existe no servidor
-        //[ProtoEnum(Name = "BarraDor", Value = 1)]
-        //[Description("Barra'Dor")]
-        //BarraDor = 1,
-        [ProtoEnum(Name = "Sumario", Value = 2)]
-        sumario,
-        [ProtoEnum(Name = "QuintaDor", Value = 4)]
-        [Description("Quinta D'Or")]
-        QuintaDor,
-        [ProtoEnum(Name = "Pronto", Value = 5)]
-        [Description("Prontolinda")]
-        Pronto,
-        [ProtoEnum(Name = "Rios", Value = 6)]
-        [Description("Rios D'Or")]
-        Rios,
-        [ProtoEnum(Name = "Norte", Value = 7)]
-        [Description("Norte D'Or")]
-        Norte,
-        [ProtoEnum(Name = "Esperança", Value = 8)]
-        [Description("Esperança")]
-        Esperanca,
-        [ProtoEnum(Name = "Bangu", Value = 9)]
+
+        [ProtoEnum(Name = "Assunção", Value = 1)]
+        [Description("Assunção")]
+        Assuncao = 1,
+        [ProtoEnum(Name = "Badim", Value = 2)]
+        [Description("Badim")]
+        Badim = 2,
+        [ProtoEnum(Name = "Bangu", Value = 3)]
         [Description("Bangu")]
-        Bangu,
-        [ProtoEnum(Name = "Copa", Value = 10)]
+        Bangu = 3,
+        //View de atendimento não existe no servidor
+        [ProtoEnum(Name = "BarraDor", Value = 4)]
+        [Description("Barra'Dor")]
+        BarraDor = 4,
+        [ProtoEnum(Name = "Brasil", Value = 5)]
+        [Description("Brasil")]
+        Brasil = 5,
+        [ProtoEnum(Name = "Copa D'Or", Value = 6)]
         [Description("Copa D'Or")]
-        Copa,
-        [ProtoEnum(Name = "SM", Value = 11)]
+        CopaDor = 6,
+        [ProtoEnum(Name = "Esperança", Value = 7)]
+        [Description("Esperança")]
+        Esperanca = 7,
+        [ProtoEnum(Name = "Israelita Albert Sabim", Value = 8)]
+        [Description("Israelita Albert Sabim")]
+        IsraelitaAlbertSabim = 8,
+        [ProtoEnum(Name = "Joari", Value = 9)]
+        [Description("Joari")]
+        Joari = 9,
+        [ProtoEnum(Name = "Niterói D'Or", Value = 10)]
+        [Description("Niterói D'Or")]
+        NiteroiDOr = 10,
+        [ProtoEnum(Name = "Norte D'Or", Value = 11)]
+        [Description("Norte D'Or")]
+        Norte = 11,
+        [ProtoEnum(Name = "Prontolinda", Value = 12)]
+        [Description("Prontolinda")]
+        Pronto = 12,
+        [ProtoEnum(Name = "Quinta D'Or", Value = 13)]
+        [Description("Quinta D'Or")]
+        QuintaDor = 13,
+        [ProtoEnum(Name = "Rede D'Or São Luiz", Value = 14)]
+        [Description("Rede D'Or São Luiz")]
+        RedeDOrSaoLuiz = 14,
+        [ProtoEnum(Name = "Rio de Janeiro", Value = 15)]
+        [Description("Rio de Janeiro")]
+        RioDeJaneiro = 15,
+        [ProtoEnum(Name = "Rios D'Or", Value = 6)]
+        [Description("Rios D'Or")]
+        RiosDor = 16,
+        [ProtoEnum(Name = "São Marcos", Value = 11)]
         [Description("São Marcos")]
-        SM
+        SaoMarcos = 17,
+        [ProtoEnum(Name = "Sumario", Value = 99)]
+        sumario = 99
     }
 }
 
