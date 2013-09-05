c3a9f44cda437ddae95501ee2e32ea0f2dec43a3
ThiagoNOliveira <eno.thiago@gmail.com>
2013-09-05 17:39:39 -0300

    Relacionamento de hospital com estado.

--- Hospital.cs
+++ Hospital.cs
@@ -8,7 +8,11 @@ namespace EHR.CoreShared
     {
         [ProtoMember(3)]
         public virtual string Name { get; set; }
+
         [ProtoMember(4)]
         public virtual string URLImage { get; set; }
+
+        [ProtoMember(5)]
+        public virtual State State { get; set; }
     }
 }
---
+++ State.cs
@@ -0,0 +1,12 @@
+﻿
+using ProtoBuf;
+
+namespace EHR.CoreShared
+{
+    [ProtoContract]
+    public class State : ValueObject
+    {
+        [ProtoMember(6)]
+        public virtual string Acronym { get; set; }
+    }
+}
